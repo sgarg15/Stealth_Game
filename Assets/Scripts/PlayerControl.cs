09b956855c7db8d8d962ad43b8cd486ec2b9065c
sgarg03 <sat.garg03@gmail.com>
2020-05-17 15:36:06 -0700

    initial Commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour {
+
+  public event System.Action OnReachedEndOfLevel;
+  public float moveSpeed = 6;
+  public float smoothMoveTime = 0.1f;
+  public float turnSpeed = 8;
+
+  float angle;
+
+  float smoothInputMagnitude;
+  float smoothMoveVelocity;
+
+  Rigidbody myRigidbody;
+  Vector3 velocity;
+  Vector3 inputDirection;
+  bool disabled;
+  // Start is called before the first frame update
+  void Start() {
+    myRigidbody = GetComponent<Rigidbody> ();
+    Guard.OnGuardHasSpottedPlayer += Disabled;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Vector3 inputDirection = Vector3.zero;
+    if(!disabled){
+     inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+    float inputMagnitude = inputDirection.magnitude;
+    smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+    float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+    angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+    velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+  }
+
+  void OnTriggerEnter(Collider hitCollider){
+    if(hitCollider.tag == "Finish"){
+      Disabled();
+      if(OnReachedEndOfLevel != null){
+        OnReachedEndOfLevel ();
+      }
+    }
+  }
+
+  void Disabled(){
+    disabled = true;
+  }
+
+  void FixedUpdate(){
+    myRigidbody.MoveRotation (Quaternion.Euler(Vector3.up * angle));
+    myRigidbody.MovePosition (myRigidbody.position + velocity * Time.deltaTime);
+  }
+
+  void OnDestroy(){
+    Guard.OnGuardHasSpottedPlayer -= Disabled;
+  }
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+  public Text scoreUI;
+  public float scoreToIncreaseBy;
+  float coinScore;
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    scoreUI.text = ((int)coinScore).ToString("D3");
+  }
+
+  void OnTriggerEnter(Collider triggerCollider){
+    if(triggerCollider.tag == "Coin") {
+      coinScore += scoreToIncreaseBy;
+      Destroy(triggerCollider.gameObject);
+    }
+  }
+
+}
